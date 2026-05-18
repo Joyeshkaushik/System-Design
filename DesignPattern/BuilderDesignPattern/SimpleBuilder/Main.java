c727a75e3c7bb9c88d30361ba4682cd386e14320
Joyesh Kaushik <173632815+Joyeshkaushik@users.noreply.github.com>
2026-05-18 20:20:40 +0530

    Implemented simple Builder Pattern

---
+++ DesignPattern/BuilderDesignPattern/SimpleBuilder/Main.java
@@ -0,0 +1,18 @@
+package BuilderDesignPattern.SimpleBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        // Using Builder Pattern (nested class)
+        HttpRequest request = new HttpRequest.HttpRequestBuilder()
+            .withUrl("https://api.example2.com")
+            .withMethod("POST")
+            .withHeader("Content-Type", "application/json")
+            .withHeader("Accept", "application/json")
+            .withQueryParams("key", "12345")
+            .withBody("{\"name\": \"Aditya\"}")
+            .withTimeout(120)
+            .build();
+
+        request.execute(); // Guaranteed to be in a consistent state
+    }
+}
